4a81688a0a17aff39621731eefe520782b025401
IsraelBosun <israelbosun1@gmail.com>
2022-12-22 14:06:56 +0100

    second commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,15 @@ import './Header.css'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <Header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Israel Bosun</h1>
+        <h5 className="text-light">
+          Fullstack Developer
+        </h5>
+      </div>
+    </Header>
   )
 }
 
